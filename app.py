335d99e09c62a50b982c9f09e12abcaebc32bb47
giovannacbs <giovannadcbs@gmail.com>
2024-05-10 08:31:07 -0300

    functional ok

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-import numpy as np
+import requests 
 
 app = Flask(__name__)
 
@@ -7,23 +7,63 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
-@app.route("/ipca.html", methods=["GET"])
+@app.route("/ipca.html", methods=["GET", "POST"])  
 def ipca():
-    periodicidade = request.args.get('periodicidade')
-    classificacao = request.args.get('classificacao')
-    
+    if request.method == "POST":
+        periodicidade = request.form.get('periodicidade')
+        classificacao = request.form.get('classificacao')
+    else:
+        periodicidade = request.args.get('periodicidade')
+        classificacao = request.args.get('classificacao')
+
     result_ipca = calc_ipca(periodicidade, classificacao)
+    if result_ipca is None:
+        return "Erro ao calcular IPCA"  
+    
+    if periodicidade == '15':
+       per = 'prévia da'
+    else:
+       per = ''
 
-    return render_template('ipca.html', result_ipca = result_ipca)
+    yy = result_ipca['infl_yy']
+    yy_txt = f"{float(yy):,.2f}".replace(",", "TEMP").replace(".", ",").replace("TEMP", ".")
+    mm = result_ipca['infl_mm']
+    mm_txt = f"{float(mm):,.2f}".replace(",", "TEMP").replace(".", ",").replace("TEMP", ".")
+    menor_valor = result_ipca['menor_cont']
+    menor_valor_txt = f"{float(menor_valor):,.2f}".replace(",", "TEMP").replace(".", ",").replace("TEMP", ".")
+    maior_valor = result_ipca['maior_cont']
+    maior_valor_txt = f"{float(maior_valor):,.2f}".replace(",", "TEMP").replace(".", ",").replace("TEMP", ".")
+    maior_grupo = result_ipca['maior_grupo'].lower()
+    menor_grupo = result_ipca['menor_grupo'].lower()
+    data = result_ipca['data_result']
+    classificacao = result_ipca['classificacao']
 
+    if menor_valor >= 0:
+       txt_menor = 'menor contribuição'
+    else:
+       txt_menor = 'maior contribuição negativa'
+
+    return render_template('ipca.html', menor_valor=menor_valor,
+                                        menor_valor_txt = menor_valor_txt,
+                                        maior_valor = maior_valor,
+                                        maior_valor_txt = maior_valor_txt,
+                                        maior_grupo = maior_grupo,
+                                        menor_grupo = menor_grupo,
+                                        yy = yy,
+                                        yy_txt = yy_txt,
+                                        mm = mm,
+                                        mm_txt = mm_txt,
+                                        data = data,
+                                        per = per,
+                                        classificacao = classificacao,
+                                        txt_menor = txt_menor)
 
 def calc_ipca(periodicidade, classificacao):
 
-  if periodicidade.lower() == "mensal":
+  if periodicidade == "mensal":
     tabela = "7060"
     variaveis = "63|66|2265"
-  elif periodicidade.lower() == "15":
+  elif periodicidade == "15":
     tabela = "7062"
     variaveis = "355|357|1120"
   else: 
@@ -56,9 +96,7 @@ def calc_ipca(periodicidade, classificacao):
           }
     
   data_result = f"{(meses[mes]).title()}/{ano}"
-    
-  #print(f"{(meses[mes]).title()}/{ano}:\n{infl_mm}% m/m \n{infl_yy}% a/a")
-    
+        
   if classificacao == "grupo":
       parametro = "7169,7170,7445,7486,7558,7625,7660,7712,7766,7786"
       nome_par = 2
@@ -72,19 +110,10 @@ def calc_ipca(periodicidade, classificacao):
   dic = {}
         
   for x in range(len(r[0]["resultados"])-1):
-      # nomes dos grupo    
       grupo = (list((r[0]["resultados"][x+1]["classificacoes"][0]["categoria"]).values())[0])[nome_par:]
-      # print(grupo)
-      # variacao mensal
-      mm = float(list((r[0]["resultados"][x+1]["series"][0]["serie"]).values())[0])
-      # print(mm)
-      # peso
+      mm = float(list((r[0]["resultados"][x+1]["series"][0]["serie"]).values())[0])      # peso
       peso = float(list((r[1]["resultados"][x+1]["series"][0]["serie"]).values())[0])
-      # print(peso)
-      # contribuicao em pontos percentuais
       cont = (peso/100)*mm
-      # print(cont)
-      # criando dicionários com contribuicoes
       dic[cont] = grupo
         
   maior_cont = sorted(dic, reverse=True)[0]
@@ -92,10 +121,6 @@ def calc_ipca(periodicidade, classificacao):
   maior_grupo = dic[maior_cont]
   menor_grupo = dic[menor_cont]
         
-  #print("------")
-  #print(f"Maior contribuição positiva de {classificacao}: {maior_grupo.lower()} ({round(maior_cont, 2)} pp.)")
-  #print(f"Menor contribuição negativa de {classificacao}: {menor_grupo.lower()} ({round(menor_cont, 2)} pp.)")
-
   return {
         "maior_cont": maior_cont,
         "menor_cont": menor_cont,
@@ -103,5 +128,6 @@ def calc_ipca(periodicidade, classificacao):
         "menor_grupo": menor_grupo,
         "infl_mm": infl_mm,
         "infl_yy": infl_yy,
-        "data_result": data_result
+        "data_result": data_result,
+        "classificacao": classificacao
     }
